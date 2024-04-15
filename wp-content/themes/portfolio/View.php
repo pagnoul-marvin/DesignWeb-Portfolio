ec38326838e1c081e37126e6a2a6be524c096651
pagnoul-marvin <marvin.pagnoul@student.hepl.be>
2024-04-15 17:58:12 +0200

    Add class view in correct file

---
+++ wp-content/themes/portfolio/View.php
@@ -0,0 +1,13 @@
+<?php
+
+class View
+{
+    public static function component(string $path, array $data = []): void
+    {
+        extract($data);
+
+        $path = str_replace('.', '/', $path);
+
+        require base_path("resources/components/{$path}.php");
+    }
+}
