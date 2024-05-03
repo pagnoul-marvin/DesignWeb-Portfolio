e5fcb76e791aac61dae29ccfbbbe7f070b56c47a
pagnoul-marvin <marvin.pagnoul@student.hepl.be>
2024-05-03 18:04:24 +0200

    contact form improved (need to be improved more)

--- wp-content/themes/portfolio/me_contacter.php
+++ wp-content/themes/portfolio/me_contacter.php
@@ -4,6 +4,86 @@ Template Name: Me contacter
 */
 ?>
 
+<?php
+require_once('database/Validator.php');
+$config = parse_ini_file('.env.local.ini');
+
+$dbname = $config['DB_NAME'];
+$username = $config['DB_USER'];
+$password = $config['DB_PASSWORD'];
+$host = $config['DB_HOST'];
+
+$errors = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (!Validator::min('firstname', 3)) {
+        $errors['firstname'] = 'Le prénom doit avoir une longueur minimale de 3 caractères.';
+    }
+
+    if (!Validator::min('lastname', 3)) {
+        $errors['lastname'] = 'Le nom doit avoir une longueur minimale de 3 caractères.';
+    }
+
+    if (!Validator::emailContainsAtSymbol($_REQUEST['mail'])) {
+        $errors['mail'] = 'L\'adresse e-mail doit contenir un "@".';
+    }
+
+    $requiredFields = ['firstname', 'lastname', 'mail', 'message'];
+    foreach ($requiredFields as $field) {
+        if (!Validator::required($field)) {
+            $errors[$field] = ucfirst($field) . ' est requis.';
+        }
+    }
+
+    if (empty($errors)) {
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $firstname = $_POST['firstname'];
+            $lastname = $_POST['lastname'];
+            $gender = $_POST['gender'];
+            $email = $_POST['mail'];
+            $message = $_POST['message'];
+
+            $sql = "INSERT INTO `wp_contact_form_entries` (`firstname`, `lastname`, `gender`, `email`, `message`) VALUES (:firstname, :lastname, :gender, :email, :message)";
+            $stmt = $conn->prepare($sql);
+
+            $stmt->bindParam(':firstname', $firstname);
+            $stmt->bindParam(':lastname', $lastname);
+            $stmt->bindParam(':gender', $gender);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':message', $message);
+            $stmt->execute();
+
+            // Envoyer l'email
+            $to = "marvinpagnoul@icloud.com";
+            $subject = "Nouveau formulaire de contact soumis";
+            $message = "Un nouveau formulaire de contact a été soumis avec les informations suivantes:\n\n";
+            $message .= "Prénom: $firstname\n";
+            $message .= "Nom: $lastname\n";
+            $message .= "Genre: $gender\n";
+            $message .= "E-mail: $email\n";
+            $message .= "Sujet: $subject\n";
+            $headers = "From: $email\r\n";
+            $headers .= "Reply-To: $email\r\n";
+            $mailSent = mail($to, $subject, $message, $headers);
+
+            if ($mailSent) {
+                echo "E-mail envoyé avec succès.";
+            } else {
+                echo "Échec de l'envoi de l'e-mail.";
+            }
+        } catch (PDOException $e) {
+            echo "La connexion à la base de données a échoué: " . $e->getMessage();
+        }
+    }
+}
+?>
+
+
+
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
 
     <?php get_header(); ?>
@@ -18,16 +98,16 @@ Template Name: Me contacter
         <section id="mail_contact" class="section">
 
             <?php component('global.titles.second_title', [
-                    'class' => '',
-                    'text' => 'Par mail'
+                'class' => '',
+                'text' => 'Par mail'
             ]); ?>
 
-            <form action="database/validation.php" method="POST" id="mail_contact_form">
+            <form action="" method="POST" id="mail_contact_form">
 
                 <fieldset>
 
                     <legend>Formulaire de contact par mail</legend>
-                    <p>Les champs dot&eacute;s d'une &laquo;&ast;&raquo; sont requis.</p>
+                    <p>Les champs dot&eacute;s d&rsquo;une &laquo;&ast;&raquo; sont requis.</p>
 
                     <?php component('global.forms.labels_and_inputs.text_input', [
                         'link' => 'firstname',
